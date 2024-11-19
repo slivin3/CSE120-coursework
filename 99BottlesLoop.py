86f309ddef3a42e8a1bc61873100abbbd9376cb1
Stephen Livingston <slivin3@users.noreply.github.com>
2024-11-18 21:39:17 -0800

    Update 99BottlesLoop.py
    Used Python F strings for value callback to the variable 'i' to alter if the reference to bottles is plural or singular from the range of 1-99.

--- 99BottlesLoop.py
+++ 99BottlesLoop.py
@@ -1,2 +1,7 @@
 ##Created by SAL on 11/18/2024
 
+for i in range(99, 0, -1):
+    if i > 1:
+        print(f'{i} bottle{"" if i==1 else "s"} of beer on the wall, {i} bottle{"" if i==1 else "s"} of beer\nTake one down and pass it around, {i-1 or "no more"} bottle{"" if i-1==1 else "s"} of beer on the wall.\n')
+else:
+    print('No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.')
