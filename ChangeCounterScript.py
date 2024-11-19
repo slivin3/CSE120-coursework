cf7e74ea9bf146502c23fb1dabf1b2bd5abe461d
Stephen Livingston <slivin3@users.noreply.github.com>
2024-11-18 21:49:36 -0800

    Create ChangeCounterScript.py
    Create file

---
+++ ChangeCounterScript.py
@@ -0,0 +1,7 @@
+##Created by SAL on 11/18/2024
+
+#Instructions: Write a program that accepts a dollar amount 0.00 and 0.99 as an input and display exact changes for this amount using the minimum number of coins.
+#For example, $0.97 shows 3 quarters, 2 dimes, and 2 pennies.
+
+
+##Note to self:**Use multiple loops of Modulus "%" in Python to find out how many of each denomination coin will be used.
